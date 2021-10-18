7ef0e1728dcc3a922178bd362d64f4877f7ee108
VanderSant <vanderson.santos@usp.br>
2021-10-18 09:37:58 -0300

    🎨  format teams.js and TeamCard.js

--- src/team-card/TeamCard.js
+++ src/team-card/TeamCard.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './TeamCard.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./TeamCard.css";
 
-function TeamCard (props) {
+function TeamCard(props) {
   const team = props.team;
   const objStyle = {
-    'background-image': `url(${team.cover})`
+    "background-image": `url(${team.cover})`,
   };
   return (
-    <div key={team.id} className='team-container'>
-      <div
-        id={team.id}
-        style={objStyle}
-        className='team'
-      >
-        <div className='info d-flex flex-column justify-content-around align-items-center'>
-          <div className='name text-center'>
-            {team.name}
-          </div>
-          <div className='description text-center px-3'>
-            {team.desc}
-          </div>
-          <div className='buttons'>
-            {button_creater(team.instagram,'fa fa-instagram fa-2x ml-1 mr-1')}
-            {button_creater(team.facebook,'fa fa-facebook-official fa-2x ml-1 mr-1')}
-            {button_creater(team.github,'fa fa-github fa-2x ml-1 mr-1')}
-            {button_creater(team.site,'fa fa-globe fa-2x ml-1 mr-1')}
-            {button_creater(team.youtube,'fa fa-youtube fa-2x ml-1 mr-1')}
+    <div key={team.id} className="team-container">
+      <div id={team.id} style={objStyle} className="team">
+        <div className="info d-flex flex-column justify-content-around align-items-center">
+          <div className="name text-center">{team.name}</div>
+          <div className="description text-center px-3">{team.desc}</div>
+          <div className="buttons">
+            {button_creater(team.instagram, "fa fa-instagram fa-2x ml-1 mr-1")}
+            {button_creater(
+              team.facebook,
+              "fa fa-facebook-official fa-2x ml-1 mr-1"
+            )}
+            {button_creater(team.github, "fa fa-github fa-2x ml-1 mr-1")}
+            {button_creater(team.site, "fa fa-globe fa-2x ml-1 mr-1")}
+            {button_creater(team.youtube, "fa fa-youtube fa-2x ml-1 mr-1")}
           </div>
         </div>
-        <img src={team.logo} alt='' />
+        <img src={team.logo} alt="" />
       </div>
     </div>
   );
 }
 
-const button_creater = (link,className) => {
-  return (link !== '')? <a href={link}><i className={className} /></a>:
-                        <a><i className={className} /></a>
+const button_creater = (link, className) => {
+  return link !== "" ? (
+    <a href={link}>
+      <i className={className} />
+    </a>
+  ) : (
+    <a>
+      <i className={className} />
+    </a>
+  );
 };
 
 TeamCard.propTypes = {
@@ -47,8 +49,8 @@ TeamCard.propTypes = {
     desc: PropTypes.string.isRequired,
     site: PropTypes.string.isRequired,
     facebook: PropTypes.string.isRequired,
-    youtube: PropTypes.string.isRequired
-  }
+    youtube: PropTypes.string.isRequired,
+  },
 };
 
 export default TeamCard;
